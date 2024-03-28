b0d60db16d891025ff3b68d84f6e4ba2f1f4562a
Gabriel Henrique do Nascimento Neres <gabrielneres@ibict.br>
2024-03-28 01:54:12 +0000

    Resolve "[Parceiro] Checar CNPJ com API externa, validar CNPJ"

--- app/src/components/formularios/utils.js
+++ app/src/components/formularios/utils.js
@@ -73,3 +73,35 @@ export function validarCPF(cpf) {
 
   return cpf[9] == digitoVerificador1 && cpf[10] == digitoVerificador2;
 }
+
+export function validarCNPJ(cnpj) {
+  cnpj = cnpj.replace(/\D/g, "");
+  let resto, digitoVerificador1, digitoVerificador2;
+  if (cnpj.length !== 14 || cnpj.match(/(\d)\1{10}/)) {
+    return false;
+  }
+
+  let validacao1 = 0,
+    validacao2 = 0;
+
+  for (let i = 0; i < 12; i++) {
+    if (i < 4) {
+      validacao1 += cnpj[i] * (5 - i);
+      validacao2 += cnpj[i] * (6 - i);
+    } else if (i == 4) {
+      validacao1 += cnpj[i] * 9;
+      validacao2 += cnpj[i] * 2;
+    } else {
+      validacao1 += cnpj[i] * (13 - i);
+      validacao2 += cnpj[i] * (14 - i);
+    }
+  }
+
+  resto = validacao1 % 11;
+  digitoVerificador1 = resto < 2 ? 0 : 11 - resto;
+
+  resto = (validacao2 + digitoVerificador1 * 2) % 11;
+  digitoVerificador2 = resto < 2 ? 0 : 11 - resto;
+
+  return cnpj[12] == digitoVerificador1 && cnpj[13] == digitoVerificador2;
+}
