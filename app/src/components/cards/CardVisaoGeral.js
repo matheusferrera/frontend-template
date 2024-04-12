716e1a9d4df161a9964e0bb0cc850895952b41f8
Matheus Ferreira de Almeida <matheusalmeida@ibict.br>
2024-04-12 14:36:50 +0000

    implementacao main admin

--- app/src/components/cards/CardVisaoGeral.js
+++ app/src/components/cards/CardVisaoGeral.js
@@ -1,62 +1,51 @@
 import React from "react";
 
-import { Card, CardContent, Grid, Typography } from "@mui/material";
+import { Card, CardContent, Skeleton, Typography } from "@mui/material";
 import PropTypes from "prop-types";
 
-const CardVisaoGeral = ({ services }) => {
+const CardVisaoGeral = ({ number, title }) => {
   return (
-    <Grid
-      container
-      spacing={1.5}
-      mt={2}
-    >
-      {Object.entries(services).map(([title, number], index) => (
-        <Grid
-          item
-          key={index}
-        >
-          {/* {service.hasChanged && (
-            <Box
-              sx={{
-                position: "relative",
-                width: "12px",
-                height: "12px",
-                backgroundColor: "primary.main",
-                borderRadius: "50%",
-                top: "10px",
-                zIndex: 10,
-              }}
-            ></Box>
-          )} */}
-          <Card sx={{ borderRadius: "8px", boxShadow: "0px 1px 6px 0px rgba(51, 51, 51, 0.16)" }}>
-            <CardContent
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "flex-start",
-                color: "primary.main",
-                paddingTop: "6px",
-                paddingBottom: "6px !important",
-                gap: "8px",
-                position: "relative",
-              }}
-            >
-              <Typography
-                variant="h4"
-                sx={{ fontSize: "22px" }}
-              >
-                {number}
-              </Typography>
-              <Typography variant="subtitle1">{title}</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
+    <Card sx={{ borderRadius: "0px", border: "1px solid #9E9E9E", width: "max-content", height: "46px" }}>
+      <CardContent
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          color: "primary.main",
+          paddingTop: "6px",
+          paddingBottom: "6px !important",
+          gap: "8px",
+          position: "relative",
+        }}
+      >
+        <Typography sx={{ fontSize: "22px", fontWeight: "bold" }}>
+          {number ? (
+            number
+          ) : (
+            <Skeleton
+              variant="circular"
+              width={30}
+              height={30}
+            ></Skeleton>
+          )}
+        </Typography>
+        <Typography sx={{ fontSize: "16px", fontWeight: "semi-bold" }}>
+          {title ? (
+            title
+          ) : (
+            <Skeleton
+              width={120}
+              height={30}
+            ></Skeleton>
+          )}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
 CardVisaoGeral.propTypes = {
-  services: PropTypes.any.isRequired,
+  number: PropTypes.number,
+  title: PropTypes.string,
 };
 export default CardVisaoGeral;
