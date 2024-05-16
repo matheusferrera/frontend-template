097dc3fc2a452d1e06a7f17f48b7aecefbec2d37
Lucas Rodrigues Costa <lucasrodrigues@ibict.br>
2024-05-16 19:25:15 +0000

    Frontend em producao

--- app/src/services/config.js
+++ app/src/services/config.js
@@ -3,7 +3,7 @@ let API_URL;
 if (window.location.hostname === "localhost") {
   API_URL = "http://localhost:8000/api/";
 } else {
-  API_URL = "http://200.130.45.104:8000/api/";
+  API_URL = "http://200.130.45.104/swagger/api/";
 }
 
 export default API_URL;
