6f4daa3225f9f76ee84d00647d1291fbdfc3fdec
Felipe Silva <felipesilva@ibict.br>
2024-05-17 16:36:00 +0000

    Resolve "[HU001] Conectar com backend"

--- app/src/pages/parceiro/PageNovaInscricaoParceiro.js
+++ app/src/pages/parceiro/PageNovaInscricaoParceiro.js
@@ -4,6 +4,7 @@ import { Container, Typography } from "@mui/material";
 
 import CardBreadcrumb from "../../components/cards/CardBreadcrumb";
 import FormParceiros from "../../components/formularios/FormParceiros";
+import parceiroService from "../../services/parceiro.service";
 
 const mapearNaturezaJuridica = valor => {
   switch (valor) {
@@ -26,50 +27,92 @@ const PageNovaInscricaoParceiro = () => {
 
   const handleSubmit = async (values, { setSubmitting }) => {
     setLoading(true);
-    return new Promise(resolve => {
-      if (
-        values.checkVagaEmprego ||
-        values.checkVagaEstagio ||
-        values.checkVagaJovem ||
-        values.checkCursos ||
-        values.checkFinanceiro ||
-        values.checkMobilidadePublico ||
-        values.checkMobilidadeParceiro
-      ) {
-        // Ajuste de naturezaJuridica para o backend
-        values.naturezaJuridica = mapearNaturezaJuridica(values.naturezaJuridica);
+    if (
+      values.checkVagaEmprego ||
+      values.checkVagaEstagio ||
+      values.checkVagaJovem ||
+      values.checkCursos ||
+      values.checkFinanceiro ||
+      values.checkMobilidadePublico ||
+      values.checkMobilidadeParceiro
+    ) {
+      // Ajuste de naturezaJuridica para o backend
+      values.naturezaJuridica = mapearNaturezaJuridica(values.naturezaJuridica);
+      const novoParceiro = {
+        dados: {
+          email: values.email,
+          cnpj: values.cnpj.replace(/[^\d]/g, ""),
+          razao_social: values.razaoSocial,
+          nome_fantasia: values.nomeFantasia,
+          cep: values.cep.replace(/[^\d]/g, ""),
+          endereco: values.endereco,
+          numero: values.numero ? values.numero : "0",
+          complemento: values.complemento ? values.complemento : "NaN",
+          bairro: values.bairro,
+          uf: values.uf,
+          cidade: values.cidade,
+          telefone: values.telefone.replace(/[^\d]/g, ""),
+          site: values.site,
+          tipo: "Empresa",
+          porte: "Pequena",
+          area_atuacao: values.areaAtuacao,
+          natureza_juridica: values.naturezaJuridica,
+          redes_sociais: values.redesSociais ? values.redesSociais : ["https://www.facebook.com/IBICTbr"],
+        },
+        representante: {
+          nome: values.nomeRepresentante,
+          cpf: values.cpf.replace(/[^\d]/g, ""),
+          telefone: values.telefoneRepresentante.replace(/[^\d]/g, ""),
+          uf: values.ufRepresentante,
+          cidade: values.cidadeRepresentante,
+        },
+        ponto_focal: {
+          nome: values.nomePontoFocal,
+          email: values.emailPontoFocal,
+          telefone: values.telefonePontoFocal.replace(/[^\d]/g, ""),
+        },
+        servico_ofertado: {
+          VEP: values.checkVagaEmprego,
+          VET: values.checkVagaEstagio,
+          VJA: values.checkVagaJovem,
+          CUR: values.checkCursos,
+          FPG: values.checkFinanceiro,
+          MPu: values.checkMobilidadePublico,
+          MPa: values.checkMobilidadeParceiro,
+        },
+      };
 
-        // TODO: mudar para a chamada no backend
-        console.log(values);
-        // Simula uma operação assíncrona
-        setTimeout(() => {
-          // Retorna a Promisse como sucesso no Timeout
-          resolve("Submissão bem-sucedida!");
-        }, 2000);
+      return parceiroService
+        .postCadastrarEmpresa(novoParceiro)
+        .then(mensagem => {
+          console.log("Sucesso:", mensagem);
+          setConfirmacaoModal(true);
+        })
+        .catch(erro => {
+          console.error(" ", erro);
+          setErroModal(true);
+        })
+        .finally(() => {
+          setSubmitting(false);
+          setLoading(false);
+        });
 
-        // Código que pode ser usado pra simular erro;
-        // Como o código não tem nenhuma condição, ao ser utilizado o erro irá ser lançado antes do Timeout (Sucesso);
-        // throw new Error("Erro simulado durante a submissão!");
-      } else {
-        // Detecta a presença de erro de preenchimento e finaliza o promise
-        setErroDoUsuarioModal(true);
-        setSubmitting(false);
-        setLoading(false);
-        console.error("Campos em branco");
-      }
-    })
-      .then(mensagem => {
-        console.log("Sucesso:", mensagem);
-        setConfirmacaoModal(true);
-      })
-      .catch(erro => {
-        console.error("", erro);
-        setErroModal(true);
-      })
-      .finally(() => {
-        setSubmitting(false);
-        setLoading(false);
-      });
+      // // Simula uma operação assíncrona
+      // setTimeout(() => {
+      //   // Retorna a Promisse como sucesso no Timeout
+      //   resolve("Submissão bem-sucedida!");
+      // }, 2000);
+
+      // Código que pode ser usado pra simular erro;
+      // Como o código não tem nenhuma condição, ao ser utilizado o erro irá ser lançado antes do Timeout (Sucesso);
+      // throw new Error("Erro simulado durante a submissão!");
+    } else {
+      // Detecta a presença de erro de preenchimento e finaliza o promise
+      setErroDoUsuarioModal(true);
+      setSubmitting(false);
+      setLoading(false);
+      console.error("Campos em branco");
+    }
   };
 
   return (
